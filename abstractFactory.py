cc1331fb85c684bbdaf9f6d0aac3d75d8c02922f
AdiNew2024 <161406371+AdiNew2024@users.noreply.github.com>
2024-02-28 00:31:23 +0000

    Abstract factory

--- abstractFactory.py
+++ abstractFactory.py
@@ -7,18 +7,32 @@ class Dog:
 	def __str__(self):
 		return "Dog"
 
+class Cat:
+
+	def speak(self):
+		return "Meow!"
+
+	def __str__(self):
+		return "Cat"
 
 class DogFactory:
 	"""Concrete Factory"""
 
 	def get_pet(self):
-		
-
+		"""Returns dog object"""
+		return Dog()
 	
 	def get_food(self):
-		
+		"""Returns dog food object"""
+		return "Dog Food!"
 
+class CatFactory:
 
+	def get_pet(self):
+		return Cat()
+
+	def get_food(self):
+		return "Cat Food!"
 
 class PetStore:
 	""" PetStore houses our Abstract Factory """
@@ -26,12 +40,13 @@ class PetStore:
 	def __init__(self, pet_factory=None):
 		""" pet_factory is our Abstract Factory """
 
-
+		self._pet_factory = pet_factory
 
 	def show_pet(self):
 		""" Utility method to display the details of the objects retured by the DogFactory """
 
-
+		pet = self._pet_factory.get_pet()
+		pet_food = self._pet_factory.get_food()
 		
 		print("Our pet is '{}'!".format(pet))
 		print("Our pet says hello by '{}'".format(pet.speak()))
@@ -40,8 +55,12 @@ class PetStore:
 
 #Create a Concrete Factory
 
+factory = CatFactory()
 
 #Create a pet store housing our Abstract Factory
 
+store = PetStore(factory)
 
 #Invoke the utility method to show the details of our pet
+
+store.show_pet()
