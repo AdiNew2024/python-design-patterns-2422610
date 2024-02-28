bab4f566f9631ac198302ead9e603665828482d7
AdiNew2024 <161406371+AdiNew2024@users.noreply.github.com>
2024-02-28 02:36:45 +0000

    Singleton

--- singleton.py
+++ singleton.py
@@ -1,12 +1,26 @@
 class Borg:
     
     """The Borg design pattern"""
-    
+    _shared_data = {}
 
-        
+    def __init__(self):
+        self.__dict__ = self._shared_data #Make an attribute dictionary
+
 
 class Singleton(Borg): 
     
     """The singleton class"""
-   
+    def __init__(self, **kwargs):
+        Borg.__init__(self)
+        self._shared_data.update(kwargs)
 
+    def __str__(self):
+        return str(self._shared_data) #Returns dictionary
+
+x = Singleton(HTTP = "Hyper Text Transfer Protocol")
+
+print(x)
+
+y = Singleton(NFT = "Non-Fungible Token")
+
+print(y)
